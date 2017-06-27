9848d9d166cda471220894092bc05d066b46c760
Pure White <danielwu1169@gmail.com>
2017-06-27 12:36:19 +0800

    bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_caching',
-    version='1.0.0',
+    version='1.1.0',
     keywords=('django', 'cache', 'caching'),
     description='A easy using django cache manager.',
     classifiers=[
